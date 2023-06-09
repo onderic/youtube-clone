6b214d685d9ff2e38beffc2e81a24c3fd2138e77
onderi <ondericollins80@gmail.com>
2023-06-09 10:49:16 +0300

    setup routers

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <!-- Your home page content here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Your component logic here
+  }
+  </script>
+  
